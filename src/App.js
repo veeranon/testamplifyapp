ad1f708efca858ac3b592a34c0c0dfaa12a79a05
veeranon <veeranon78@gmail.com>
2020-08-26 14:05:01 +0700

    'changes-for-v2'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
-      </header>
+      </header>  
     </div>
   );
 }
